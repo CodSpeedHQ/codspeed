8ab7c343d890906aa059700befc7facf56ccb51e
Guillaume Lagrange <guillaume@glagrange.eu>
2026-02-12 11:32:43 +0100

    feat: skip tests requiring sudo if `GITHUB_ACTIONS` is not set
    Also add a rebuild trigger to make it easier to run
    GITHUB_ACTIONS=1 cargo test` locally.
    
    We could have a better trigger, but this will do for now.

--- crates/memtrack/build.rs
+++ crates/memtrack/build.rs
@@ -3,6 +3,11 @@ use std::{env, path::PathBuf};
 
 #[cfg(feature = "ebpf")]
 fn build_ebpf() {
+    // Force a rebuild of the test target to be able to run the full test suite locally just by
+    // setting GITHUB_ACTIONS=1 in the environment.
+    // This is because `test_with` is evaluated at build time
+    println!("cargo::rerun-if-env-changed=GITHUB_ACTIONS");
+
     use libbpf_cargo::SkeletonBuilder;
 
     println!("cargo:rerun-if-changed=src/ebpf/c");
