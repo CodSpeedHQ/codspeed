8b27896ee6b9226327ed6c39e8de82c0b8c85520
Guillaume Lagrange <guillaume@glagrange.eu>
2026-03-10 16:18:40 +0100

    --wip-- [skip ci]

--- src/bin/parse_perf_file.rs
+++ src/bin/parse_perf_file.rs
@@ -2,7 +2,9 @@ use codspeed_runner::executor::wall_time::perf::parse_perf_file::{self, PidFilte
 
 fn main() {
     codspeed_runner::init_local_logger().unwrap();
-    let perf_file_path = "/home/guillaume/cod-2314/profile.pPMUwlf7Pu.out/perf.pipedata";
+    // let perf_file_path = "/home/guillaume/cod-2314/profile.pPMUwlf7Pu.out/perf.pipedata";
+    // let perf_file_path = "/home/guillaume/repro-perf-event-size/out.pipedata";
+    let perf_file_path = "/home/guillaume/repro.out.pipedata";
     let pid_filter = PidFilter::All;
     parse_perf_file::parse_for_memmap2(perf_file_path, pid_filter).unwrap_or_else(|e| {
         eprintln!("Failed to parse perf file: {e}");
