f24b97a05521964d03734167784adbe2bc895140
Guillaume Lagrange <guillaume@glagrange.eu>
2026-07-28 17:25:56 +0200

    Revert "chore: make tests run release builds of the binaries"
    This reverts commit d81db8da18e0a5ecc4af8171c66e138901a369e0.

--- src/executor/tests.rs
+++ src/executor/tests.rs
@@ -127,7 +127,6 @@ fi
             let path = escargot::CargoBuild::new()
                 .package(package)
                 .bin(bin)
-                .release()
                 .current_target()
                 .run()
                 .unwrap_or_else(|e| panic!("failed to build the {bin} binary: {e}"))
